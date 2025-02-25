7967cd2d21bc7ab24aa8c66d2d988b1ddd6c94e6
l.kabilka <luca.kabilka2@vorwerk.de>
2025-02-25 07:42:03 +0100

    first main push

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.3")
+    implementation("io.ktor:ktor-server-netty:2.3.3")
+    implementation("io.ktor:ktor-server-html-builder:2.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.9.1")
+    implementation("io.ktor:ktor-server-call-logging-jvm:2.3.3")
+    implementation("io.ktor:ktor-server-default-headers-jvm:2.3.3")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
